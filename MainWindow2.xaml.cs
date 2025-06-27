3878fa9c7691f5c11012f23b4ea7d22e076e5e04
Tombo0304 <david.marinruiz@ucr.ac.cr>
2025-06-27 11:55:12 -0600

    MainWindow2 listo - David Marín

---
+++ MainWindow2.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Steady_Management_App
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow2.xaml
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+        }
+
+        private void Salir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ConsultarClientes_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContent.Content = new ClientesListForm();
+        }
+
+        private void ConsultarOrdenes_Click(object sender, RoutedEventArgs e)
+        {
+            //MainContent.Content = new OrdenesListForm();
+        }
+    }
+}
